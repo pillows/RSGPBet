b8722d33d418e7220240c3ac4ce30cbeea75ead6
Frankie <frankie@lenovo-1.(none)>
2014-05-15 23:02:46 -0400

    fox

--- login.py
+++ login.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Blueprint, render_template, flash, redirect, request, session
 from mongo import db
 import protect
@@ -10,24 +9,10 @@ def login_():
     if "session" in session:
         return redirect("/")
 
-=======
-from flask import Blueprint, render_template, request, session, redirect, flash
-import protect
-from mongo import db
-
-login = Blueprint("login", __name__, template_folder="templates", static_folder="static")
-
-@login.route("/login/", methods=['GET', 'POST'])
-def _login():
-    if "login" in session:
-        return redirect("/dashboard/")
-    
->>>>>>> 4be3d0fd031b41e2bf30d3d442b05ca3c16710a6
     if request.method == "POST":
         email = request.form['email']
         password = protect.protect(request.form['password'])
         check = db.members.find_one({"email":email, "password":password})
-<<<<<<< HEAD
         if not check:
             flash("Invalid Login")
             return redirect("/login/")
@@ -35,14 +20,3 @@ def _login():
             session['login'] = email
             return redirect("/")
     return render_template("login.html")
-=======
-        if check:
-            session['login'] = username
-            if request.form.get("remember"):
-                session.permanent = True
-            return redirect("/dashboard/")
-        else:
-            flash("Login Failed")
-        
-    return render_template("login.html")
->>>>>>> 4be3d0fd031b41e2bf30d3d442b05ca3c16710a6
--- register.py
+++ register.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 from flask import flash, Blueprint, render_template, session, request, redirect
 from mongo import db
 import protect
@@ -29,52 +29,5 @@ def register_():
             db.members.insert({"email":email, "password":password, "rsname":rsname, "wallet":0, "admin":False})
             session['login'] = email
             return redirect("/")
-=======
-from flask import Blueprint, render_template, request, session, redirect, flash
-import protect
-from mongo import db
-import os
-import time
-import uuid
-import random
-import re
-
-register = Blueprint("register", __name__, template_folder="templates", static_folder="static")
-
-@register.route("/register/", methods=['GET', 'POST'])
-def _register(plan_choice=None):
-    if "login" in session:
-        return redirect("/dashboard/")
-
-    if request.method == "POST":
-        runescape = request.form['rsname']
-        password = protect.protect(request.form['password'])
-        confirm = protect.protect(request.form['confirmpassword'])
-        email = request.form['email']
-        
-        check = re.findall("^[+a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$", email)
-        if not check:
-            flash("You need to enter a valid email.")
-            return redirect("/register/")
-        if not request.form['password'] or not name or not email:
-            flash("You must fill out all the fields.")
-            return rendirect("/register/")
-        if password != confirm:
-            flash("Passwords did not match")
-        elif db.members.find_one({"email":email}):
-            flash("Email already taken")
-        else:
-            db.members.insert({"name":name, "password":password, "email":email, "rsname":runescape, "wallet":0})
-            session['login'] = email
-            return redirect("/dashboard/")
-            
-
-            #Plans: 1 - Free 10mb; 2 - $10 500mb; 3 - $15 1GB; 4 - $20 2GB; 5 - $50 5GB; 6 - $100 10gb 
-            #If timestamp=0 then plan is free
-            
-            ###Stripe Block Code###
-
-            ###End Block Code###
->>>>>>> 4be3d0fd031b41e2bf30d3d442b05ca3c16710a6
 
     return render_template("register.html")
